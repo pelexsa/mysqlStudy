622c0d309ab6da0a9b1256bafb2ee6923c305901
minhyeok <pelexsa@naver.com>
2021-02-07 21:32:20 +0900

    SwapSalary

---
+++ mysqlStudy/src/main/java/com/mysql/mysqlStudy/LeetCode/SwapSalary.java
@@ -0,0 +1,10 @@
+package mysqlStudy.src.main.java.com.mysql.mysqlStudy.LeetCode;
+
+public class SwapSalary {
+    UPDATE SALARY
+    SET sex =
+            CASE sex
+    WHEN 'm' THEN 'f'
+    WHEN 'f' THEN 'm'
+    END;
+}
